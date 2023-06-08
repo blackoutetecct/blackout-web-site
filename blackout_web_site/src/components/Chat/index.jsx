3a5427d96095620b2cb1212e2cfcae5947bf3e2b
WiliamMeloMota <102825885+WiliamMeloMota@users.noreply.github.com>
2023-06-07 23:01:02 -0300

    feat(chat): Adicionado opção de finalizar um chat

--- blackout_web_site/src/components/Chat/index.jsx
+++ blackout_web_site/src/components/Chat/index.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import './style.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./style.css";
 
-import ChatImage from '../../images/chat2.svg';
-import { api } from '../../libs/axios';
-import Message from '../Message';
+import ChatImage from "../../images/chat2.svg";
+import CloseChatImage from "../../images/closeChat.png";
+import { api } from "../../libs/axios";
+import Message from "../Message";
 
 export default function Chat() {
   // Refs
@@ -17,23 +18,27 @@ export default function Chat() {
   const [messages, setMessages] = useState();
   const [userId, setUserId] = useState("");
   const [userMessage, setUserMessage] = useState("");
+  const [endConversation, setEndConversation] = useState(false);
 
   // Função para enviar mensagem
   const enviarMensagem = useCallback(async () => {
     setUserMessage("");
 
-    await api.post(`message/send?response=f906ca07-e369-4b2f-999e-26dac2f5c8e9`, {
-      text: userMessage,
-      sender: userId
-    });
+    await api.post(
+      `message/send?response=f906ca07-e369-4b2f-999e-26dac2f5c8e9`,
+      {
+        text: userMessage,
+        sender: userId,
+      }
+    );
 
     // Busca todas as mensagens e aramazena somente as informações necessarias
-    let allMessages = await api.get('message/all');
-    allMessages = allMessages.data.map(message => {
+    let allMessages = await api.get("message/all");
+    allMessages = allMessages.data.map((message) => {
       return {
         time: message.time,
         text: message.text,
-        sender: message.sender
+        sender: message.sender,
       };
     });
     setMessages(allMessages);
@@ -47,12 +52,12 @@ export default function Chat() {
   useEffect(() => {
     // Busca mensagens a cada segundo
     const buscaMensagensACadaSegundo = setInterval(async () => {
-      let allMessages = await api.get('message/all');
-      allMessages = allMessages.data.map(message => {
+      let allMessages = await api.get("message/all");
+      allMessages = allMessages.data.map((message) => {
         return {
           time: message.time,
           text: message.text,
-          sender: message.sender
+          sender: message.sender,
         };
       });
       setMessages(allMessages);
@@ -61,48 +66,67 @@ export default function Chat() {
     // Recupera o ID do usuário ou cria um novo
     (async () => {
       try {
-        let recovered_user_id = localStorage.getItem('blackout_chat_user_id');
+        let recovered_user_id = localStorage.getItem("blackout_chat_user_id");
         if (recovered_user_id) {
           setUserId(recovered_user_id);
           return;
         }
-        const response = await api.post('user/save', {});
+        const response = await api.post("user/save", {});
         const { id } = response.data;
-        localStorage.setItem('blackout_chat_user_id', id);
+        localStorage.setItem("blackout_chat_user_id", id);
         setUserId(id);
       } catch (error) {
         console.error(error);
       }
     })();
 
-    function handleClick(){
-        chatRef.current.classList.toggle('chatbox--active');
-      };
+    function handleClick() {
+      chatRef.current.classList.toggle("chatbox--active");
+    }
 
     // Event listener para abrir/fechar o chat
-    botaoAbrirChatRef.current.addEventListener('click', handleClick);
+    botaoAbrirChatRef.current.addEventListener("click", handleClick);
 
     // Limpeza dos intervalos quando o componente é desmontado
     return () => {
       clearInterval(buscaMensagensACadaSegundo);
-      botaoAbrirChatRef.current.removeEventListener('click', handleClick)
+      botaoAbrirChatRef.current.removeEventListener("click", handleClick);
     };
   }, []);
 
   useEffect(() => {
     // Função para enviar mensagem ao pressionar Enter
     const handleKeyPress = (event) => {
-      if (event.key === 'Enter') {
+      if (event.key === "Enter") {
         enviarMensagem();
       }
     };
 
-    inputMensagemRef.current.addEventListener('keypress', handleKeyPress);
+    inputMensagemRef.current.addEventListener("keypress", handleKeyPress);
 
     return () => {
-      inputMensagemRef.current.removeEventListener('keypress', handleKeyPress);
+      inputMensagemRef.current.removeEventListener("keypress", handleKeyPress);
     };
   }, [userId, userMessage, enviarMensagem]);
+
+  async function tentaFinalizarChat() {
+    setEndConversation(true)
+  }
+
+  function cancelarFinalizacaoChat(){
+    setEndConversation(false)
+  }
+
+  async function confirmarFinalizacaotoChat(){
+    const deleteAdminMessages = await api.delete(`message`, {
+      params: { userId: "79df6165-6922-45a6-b99e-0ed2b1cdcf87" },
+    });
+    const deleteUserMessages = await api.delete(`message`, {
+      params: { userId },
+    });
+    setEndConversation(false)
+    chatRef.current.classList.remove("chatbox--active")
+  }
 
   return (
     <div className="container">
@@ -110,21 +134,38 @@ export default function Chat() {
         <div className="chatbox__support" ref={chatRef}>
           <div className="chatbox__header">
             <div className="chatbox__image--header">
-              <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-5--v1.png" alt="user" />
+              <img
+                src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-5--v1.png"
+                alt="user"
+              />
             </div>
             <div className="chatbox__content--header">
               <h4 className="chatbox__heading--header">Fale conosco</h4>
-              <p className="chatbox__description--header">Olá! como posso te ajudar?</p>
+              <p className="chatbox__description--header">
+                Olá! Como posso te ajudar?
+              </p>
             </div>
+            {messages && messages.length > 0 && <button className="chatbox__close--header" onClick={tentaFinalizarChat}>
+              <img src={CloseChatImage} alt="Fechar" />
+            </button>}
           </div>
           <div className="chatbox__messages" ref={divMensagensRef}>
-            {messages && messages.map((message, index) => (
-              <Message
-                message={message.text}
-                sender={message.sender === userId ? "me" : "support"}
-                key={index}
-              />
-            ))}
+            {messages && <>
+              {!endConversation ? messages.map((message, index) => (
+                <Message
+                  message={message.text}
+                  sender={message.sender === userId ? "me" : "support"}
+                  key={index}
+                />
+              )) : <div className="confirm_modal--messages">
+              <p className="confirm_modal--messages text">Tem certeza que deseja finalizar a conversa?</p>
+              <div className="confirm_modal--messages button-container">
+                <button className="confirm_modal--messages button" onClick={cancelarFinalizacaoChat}>Cancelar</button>
+                <button className="confirm_modal--messages button" onClick={confirmarFinalizacaotoChat}>Confirmar</button>
+              </div>
+            </div> }
+              </>
+              }
           </div>
           <div className="chatbox__footer">
             <input
