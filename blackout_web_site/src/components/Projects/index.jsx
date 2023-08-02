31e372c47bd1c986d00ef6310783eac99cc7a0fe
Your Name <you@example.com>
2023-08-02 09:04:50 -0300

    Melhorado estilo dos projetos

--- blackout_web_site/src/components/Projects/index.jsx
+++ blackout_web_site/src/components/Projects/index.jsx
@@ -1,7 +1,8 @@
 import './style.css'
 
 
-import project1 from '../../images/phone.png'
+import project1 from '../../images/sustenapp-logo.png'
+import project2 from '../../images/eteclogo.png'
 
 export default function Projects() {
 
@@ -22,14 +23,14 @@ export default function Projects() {
                     <a href="/">Saiba mais</a>     
                     </div>
 
-                <div className="project"> 
-                    <div><img src={project1} alt="Projeto" /></div>
+                <div className="project project2"> 
+                    <div><img src={project2} alt="projeto 2" /></div>
 
                     <h3> Dev. Watch </h3>
 
                     <p> Projeto didático desenvolvido com o intuito de testar nossas habilidades criando um E-commerce funcional de relógios de luxo como tema central.  </p>
 
-                    <a href="https://github.com/kaiquesouzasantos/etec199-2022-pw2-projetoFinal">Saiba mais</a>     
+                    <a href="https://github.com/kaiquesouzasantos/etec199-2022-pw2-projetoFinal" target="_blank">Saiba mais</a>     
                     </div>  
             </div>
         </section>
